d13b26fab35f96ef13f96fa19e48c7bf09b063e9
BobiSabev <borislav.sabev@aiesec.net>
2018-02-09 18:44:11 +0100

    add stubs for assignment 2

---
+++ 2_Hangman/src/hangman/Gallows.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman;
+
+/**
+ *
+ * @author Borislav
+ */
+public class Gallows {
+    /*
+    word - get random word from the text
+    userGuess - init to .... with the size of word
+    numTries - int for how many tries it has
+    TOTAL_GUESSES = 10
+    */
+    
+    /**
+     * Takes a guessed letter and updates userGuess 
+     * @param letter guessed letter
+     */
+    public void guess(char letter){
+        
+    }
+    
+    /**
+     * Compare word and userGuess
+     * @param guessed if the userGuess is same as word
+     */
+    public boolean isGuessed(){
+        return false;
+    }
+}
