7cb1a3609305ab27e3f597fc21a78a25351fcb40
BobiSabev <borislav.sabev@aiesec.net>
2018-02-23 09:34:13 +0100

    ShapeComperator tested and working with all shapes and modes

--- 3_Shapes/src/pkg3_shapes/Main.java
+++ 3_Shapes/src/pkg3_shapes/Main.java
@@ -29,38 +29,15 @@ public class Main {
         Execute command on case:
         */
         
-        Shape[] shapes = {new Circle(0,1,2), 
-                          new Circle(1,0,1),
-                          new Rectangle(0,0,5,5),
-                          new Rectangle(1,0,10,10)};
+        Shape[] shapes = {new Circle(4,4,2),
+                          new Rectangle(1,2,5,7),
+                          new Rectangle(2,1,5,5),
+                          new Rectangle(3,0,5,9)
+        };
         
         for(Shape sh : shapes){
             System.out.println(sh.toString());
         }
-        System.out.println(shapes[2].getLeftBorder() + " " + shapes[2].getBottomBorder());
-        shapes[2].moveTo(1,1);
-        System.out.println(shapes[2].getLeftBorder() + " " + shapes[2].getBottomBorder());
-        
-        System.out.println(shapes[0].toString());
-        
-        
-        System.out.println("\n\n\n\n\n");
-        
-        String s = "circle 5.6 6.6";
-        String[] split = s.split(" ");
-        for(String s1 : split)
-            System.out.println(s1);
-        
-        
-        
-        Circle c = new Circle(5,5,2);
-        System.out.println(c.getX());
-        c.moveTo(5, 5);
-        System.out.println(c.getX());
-        System.out.println(c.getArea());
-        System.out.println(c.getBottomBorder());
-        
-        
         
         
         
@@ -70,12 +47,13 @@ public class Main {
         Arrays.sort(shapes, comperator);
         Arrays.sort(fruits);
         
-        for(Shape shape : shapes){
-            System.out.println("\nLeft " + shape.getLeftBorder());
-            System.out.println("Bottom " + shape.getBottomBorder());
-            System.out.println("Area " + shape.getArea());
+        System.out.println("\n\n");
+        for(Shape sh : shapes){
+            System.out.println(sh.toString());
         }
        
     }
+    
+    
     
 }
