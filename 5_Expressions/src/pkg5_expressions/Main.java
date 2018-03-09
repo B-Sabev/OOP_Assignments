93eb474835f789ec2d31e66dc72e995618782bc1
BobiSabev <borislav.sabev@aiesec.net>
2018-03-09 09:36:04 +0100

    Test class added

--- 5_Expressions/src/pkg5_expressions/Main.java
+++ 5_Expressions/src/pkg5_expressions/Main.java
@@ -19,35 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
       
-       
-       ArrayList<Expression> expressions = new ArrayList<>();
-       expressions.add( mul(con(10), con(20)) );
-       expressions.add( mul(con(1), var("x")) );
-       expressions.add( mul(con(0), var("x")) );
-       expressions.add( mul(var("y"), con(1)) );
-       expressions.add( mul(var("y"), con(0)) );
-       expressions.add( neg(var("x")) );
-       expressions.add( neg(add(con(10), con(20))));
-       expressions.add( add(add(con(10), con(20)), add(con(10), con(20))));
-       expressions.add( add(neg(con(10)), neg(con(20))));
-       expressions.add( add( add(add(con(10),con(10)), add(add(add(con(10), con(20)), add(con(10), con(20))), con(10))), con(10)));
-       expressions.add( mul(con(20), neg(add(mul(con(20), con(10)),add(con(20), con(10))))) );
-       
-       
-       expressions.add( mul(var("x"), var("y")) );
-       
-       Map<String, Double> store = new HashMap<>();
-       store.put("x", 12.);
-       store.put("y", 3.);
-       
-       
-       for(Expression e : expressions){
-           System.out.println("toString: " + e.toString());
-           System.out.println("eval:     " + e.eval(store));
-           System.out.println("optimize: " + e.optimize());
-           System.out.println();
-       }
-       System.out.println(store.toString());
+       (new TestExpressions()).test();
        
        
        /*
