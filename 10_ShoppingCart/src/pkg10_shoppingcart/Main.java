fa45a7f44a222be9213c5d9bdfcc71e6f950f978
BobiSabev <borislav.sabev@aiesec.net>
2018-04-24 11:52:32 +0200

    add project descriptions for both projects

--- 10_ShoppingCart/src/pkg10_shoppingcart/Main.java
+++ 10_ShoppingCart/src/pkg10_shoppingcart/Main.java
@@ -15,7 +15,44 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+            class ShoppingCart (empty at first)
+                add/removeItem()
+                abstract boolean pay(double amount) - implement with strategy pattern
+                computeCost() item price + shipping
+                
+            class Item
+                String description
+                double price
+                double shippingCost() - abstract method
+            
+        
+            Payment
+                changePaymentMethod()
+        
+                IDeal (default payment method)
+                    bank
+                    account number
+                    pin number
+                CreditCard
+                    card number
+                    expiration date
+                PayPal
+                    e-mail
+                    password
+        
+            All items with equal shiping cost - add the shipping cost only once
+                water melon (price 4,50 and shipping 6,75)
+                wine glasses (price 8,50 and shipping 6,75)
+                washing machines (price 499 and shipping 30,00)
+                    
+            Add a suitable constructor to each class.
+            You only have to implement one of the concrete items and payment methods. Design
+            the methods as simple as possible. It is for instance suWcient for the method
+            pay to print out the payment method and the total price.    
+                
+        
+        */
     }
     
 }
--- 10_icecream/src/pkg10_icecream/Main.java
+++ 10_icecream/src/pkg10_icecream/Main.java
@@ -15,7 +15,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Exercise description:
+            abstact class Ice
+                protected String description - give "unknow ice" by default
+                abstract getPrice()
+            VanillaIce and YoghurtIce derived from Ice
+                add suitable desription
+                VI is 150 cents, YIis 200 cents
+            
+            abstract Topping extends Ice
+                    attribute Ice - store the garnished ice
+            Toppings derived from Topping - 
+                       WhippedCream (50 cents), 
+                       ChocolateDip(30 cents), 
+                       Sprinkles (0 cents , free)
+        
+        generate example icecreams, printing their description and price (class with static methods, or in main)
+                       
+        
+        
+        
+        
+        
+        */
     }
     
 }
