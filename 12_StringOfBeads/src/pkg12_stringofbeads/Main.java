5914cad4bdd8e51a3c802de9644dbf901987f436
BobiSabev <borislav.sabev@aiesec.net>
2018-05-21 13:21:52 +0200

    working version

--- 12_StringOfBeads/src/pkg12_stringofbeads/Main.java
+++ 12_StringOfBeads/src/pkg12_stringofbeads/Main.java
@@ -1,12 +1,13 @@
-
 package pkg12_stringofbeads;
 
+import java.util.List;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -17,83 +18,59 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
-/*
-TODO 
-    improve the code to have the last bead follow the mouse
-    the last bead should be always at the center of the mouse
-    and the line following the bead
-
-*/
-
 /**
  *
- * @author Borislav
+ * @author Borislav Sabev s4726863, Austin Atchley s1016930
  */
 public class Main extends Application {
     
-    private static final double START_X = 0, 
-                                START_Y = 0;
+    private static final double BEAD_SIZE = 10;
     private static final int MAX_RGB_VAL = 255,
                              MIN_RGB_VAL = 0;
-    private int numBeads = 0;
+   
     
 
-    
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
-        SimpleDoubleProperty MAX_RGB = new SimpleDoubleProperty(255);
-        SimpleIntegerProperty numberOfBeads = new SimpleIntegerProperty(1);
-        SimpleDoubleProperty gradient = new SimpleDoubleProperty();
-        gradient.bind(MAX_RGB.divide(numberOfBeads));
-     
-        
+       
+        // Create line and first bead in a group
         Group group = new Group();
-        Pane pane = new Pane(group);
-
-        pane.setPrefWidth(300);
-        pane.setPrefHeight(300);
-
         group.setTranslateX(0);
         group.setTranslateY(0);
+        group.getChildren().add(new Line(0, 0, 50, 50));
+        group.getChildren().add(new Circle(0, 0, BEAD_SIZE, Color.BLUE));
+        group.getChildren().get(1).setLayoutX(50);
+        group.getChildren().get(1).setLayoutY(50);
         
-        group.getChildren().add(new Circle(50, 50, 10, Color.BLUE));
-        /*
-        pane.setOnMouseEntered(e -> {
-      
-            group.getChildren().get(0).setLayoutX(e.getX());
-            group.getChildren().get(0).setLayoutY(e.getY());
+        
+        
+        Pane pane = new Pane(group);
+        pane.setPrefWidth(300);
+        pane.setPrefHeight(300);
+       
+        pane.setOnMouseEntered(e -> { 
+            alignBeads(group, e.getX(), e.getY());
+            e.consume();
         });
-        */
+       
         pane.setOnMouseMoved(e -> {
-            group.getChildren().get(0).setLayoutX(e.getX());
-            group.getChildren().get(0).setLayoutY(e.getY());
+            alignBeads(group, e.getX(), e.getY());
             e.consume();
         });
         
+        // create new bead, repaint and align the beads
         pane.setOnMouseClicked(e -> {
-            numBeads++;
-            numberOfBeads.set(numberOfBeads.get() + 1);
-            System.out.println(numberOfBeads.getValue() + " " + gradient.doubleValue());
-            
-            repaintBeads(gradient.doubleValue());
-            
-            ///colorAndAlignBeads(group, e.getX(), e.getY());
-            
-            /*
-            group.getChildren().add(new Circle(e.getX(), //+ group.getLayoutX(),
-                                               e.getY(), //+ group.getLayoutY(),
-                                                10));
-            colorBeads(group);
-            alignBeads(e, group);
-            */
+            group.getChildren().add(new Circle(0, 0, BEAD_SIZE, Color.BLUE));
+            double grad = MAX_RGB_VAL / ((double) group.getChildren().size() - 1);
+            repaintBeads(group, grad);
+            alignBeads(group, e.getX(), e.getY());
             e.consume();
         });
         
         Scene scene = new Scene(pane, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("String Of Beads");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -105,32 +82,30 @@ public class Main extends Application {
         launch(args);
     }
     
-    private void colorAndAlign2(Group group, DoubleProperty mouseXProp, DoubleProperty mouseYProp){
-        
-    }
     
-    
-    private void colorAndAlignBeads(Group group, double mouseX, double mouseY) {
-        group.getChildren().clear(); // delete all nodes in the group
-        int gradient = MAX_RGB_VAL / numBeads; // gradient of the color
-        double dx = (mouseX - START_X) / numBeads;
-        double dy = (mouseY - START_Y) / numBeads;
-        
-        for(int i=0; i<numBeads; i++){
-            Circle bead = new Circle(START_X + dx * ((double)i+1),
-                                     START_Y + dy * ((double)i+1),
-                                     10,
-                                     Color.rgb(MAX_RGB_VAL - gradient * i, 
-                                               MIN_RGB_VAL, 
-                                               MIN_RGB_VAL + gradient * i));
-            group.getChildren().add(bead);
+    private void alignBeads(Group group, double mouseX, double mouseY){
+        int numBeads = group.getChildren().size() - 1;
+        double dx = mouseX /((double) numBeads);
+        double dy = mouseY  / ((double) numBeads);
+        ((Line) group.getChildren().get(0)).setEndX(mouseX);
+        ((Line) group.getChildren().get(0)).setEndY(mouseY);
+        // iterate only over the beads
+        for(int i=1; i<numBeads+1; i++){
+            if( group.getChildren().get(i) instanceof Circle ){
+                group.getChildren().get(i).setLayoutX(dx * i);
+                group.getChildren().get(i).setLayoutY(dy * i);
+            }   
         }
-        group.getChildren().add(new Line(START_X, START_Y, mouseX, mouseY));
-    
-    }
-
-    private void repaintBeads(double doubleValue) {
-        
     }
     
+    private void repaintBeads(Group group, double gradient) {
+        List<Node> nodes = group.getChildren();
+        for(int i=0; i<nodes.size(); i++){
+           if (nodes.get(i) instanceof Circle){
+               ((Circle) nodes.get(i)).setFill(Color.rgb(MAX_RGB_VAL - ((int) gradient * i), 
+                                                         MIN_RGB_VAL, 
+                                                         MIN_RGB_VAL + (int) gradient * i));
+           }
+        }   
+    }  
 }
